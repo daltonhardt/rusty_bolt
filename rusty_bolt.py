ca7a59c570247caaf490cd262c6d0478f6730b8e
Dalton <61704761+daltonhardt@users.noreply.github.com>
2024-07-17 20:21:36 +0200

    Update rusty_bolt.py

--- rusty_bolt.py
+++ rusty_bolt.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 from tempfile import NamedTemporaryFile
 
+
 # Início do App
 st.set_page_config(layout='wide')
 
@@ -23,28 +24,31 @@ if arquivo is not None:
         # file_name = os.path.splitext(f.name)[0]
         file_name = f.name
         # print('file_name:', file_name)
-        original = cv.imread(file_name)
+        original = cv.imread(file_name, cv.IMREAD_UNCHANGED)
+
     st.sidebar.image(arquivo, caption='Arquivo Original')
     rows, cols, channels = original.shape
     original_tot_pixels = original.size
-    str_shape = f'rows,cols,channels,total: {rows},{cols},{channels},{original_tot_pixels}'
+    str_shape = f'rows, cols, channels, total:\n {rows}, {cols}, {channels}, {original_tot_pixels}'
     st.sidebar.text(str_shape)
-    print(f'Original rows,cols,channels,total: {rows},{cols},{channels}, {original_tot_pixels}')
 
-    # Opções avançadas de escolha
-    st.sidebar.title('Ampliar imagem:')
-    tam_pixels = st.sidebar.slider('Tamanho da imagem [pixels]:', min_value=150, max_value=500, value=rows)
     # redimensionando a imagem para X pixels e mantendo o aspect ratio
     file_bytes = np.asarray(bytearray(arquivo.read()), dtype=np.uint8)
     image_decoded = cv.imdecode(file_bytes, cv.IMREAD_ANYCOLOR)
-    razao = tam_pixels / image_decoded.shape[1]
-    dim = (tam_pixels, int(image_decoded.shape[0] * razao))
+    # razao = tam_pixels / image_decoded.shape[1]
+    # dim = (tam_pixels, int(image_decoded.shape[0] * razao))
+    razao = 250 / image_decoded.shape[1]
+    dim = (250, int(image_decoded.shape[0] * razao))
     arquivo = cv.resize(image_decoded, dim, interpolation=cv.INTER_CUBIC)
     original = cv.resize(image_decoded, dim, interpolation=cv.INTER_CUBIC)
 
     # convert to RGB
     img_rgb = cv.cvtColor(original, cv.COLOR_BGR2RGB)
-    pixel_colors = img_rgb.reshape((np.shape(img_rgb)[0]*np.shape(img_rgb)[1], 3))
+
+    # image_no_background = remove_background(img_rgb)
+    # st.image(image_no_background, caption='Imagem sem fundo')
+
+    pixel_colors = img_rgb.reshape((np.shape(img_rgb)[0] * np.shape(img_rgb)[1], 3))
     # pixel values
     norm = colors.Normalize(vmin=-1., vmax=1.)  # normalize all pixels
     norm.autoscale(pixel_colors)
@@ -55,9 +59,9 @@ if arquivo is not None:
 
     # Opções para HSV
     st.sidebar.title('HSV custom:')
-    th_hue = st.sidebar.slider('Hue Threshold:', min_value=0, max_value=255, value=(0, 45))
-    th_sat = st.sidebar.slider('Saturation Threshold:', min_value=0, max_value=255, value=(45, 255))
-    th_val = st.sidebar.slider('Value Threshold:', min_value=0, max_value=255, value=(0, 255))
+    th_hue = st.sidebar.slider('Hue threshold:', min_value=0, max_value=255, value=(0, 45))
+    th_sat = st.sidebar.slider('Saturation threshold:', min_value=0, max_value=255, value=(45, 255))
+    th_val = st.sidebar.slider('Value threshold:', min_value=0, max_value=255, value=(0, 255))
     th_hue_min, th_hue_max = th_hue
     th_sat_min, th_sat_max = th_sat
     th_val_min, th_val_max = th_val
@@ -77,8 +81,6 @@ if arquivo is not None:
     col1, col2, col3 = st.columns(3)
     with col1:
         st.image(arquivo, caption='Original BGR-RGB')
-    # with col2:
-    #     st.image(imagem, caption='Imagem HSV')
     with col2:
         st.image(mask, caption='Máscara')
     with col3:
@@ -87,19 +89,38 @@ if arquivo is not None:
     r, c, ch = masked_bgr.shape
     summed = np.sum(masked_bgr, axis=2)
     tot_size = masked_bgr.size
-    print('Arquivo Masked width, height, channels, total:', c, r, ch, tot_size)
+    # print(f'Arquivo Masked width, height, channels, total: {r}, {c}, {ch}, {tot_size}')
     mask_pixels = cv.countNonZero(mask)
-    print('mask_pixels=', mask_pixels)
-    area_oxidada = round(mask_pixels * 100 / original_tot_pixels, 2)
+    # print('mask_pixels=', mask_pixels)
+    area_oxidada = round(mask_pixels * 400 / original_tot_pixels, 2)
     print(f'Area oxidada = {area_oxidada}%')
+    st.sidebar.metric('Área Oxidada Estimada', f'{area_oxidada}%')
 
-    # plot 3d projetion
+    # plot 3d projection
     h, s, v = cv.split(imagem)
     hf, sf, vf = h.flatten(), s.flatten(), v.flatten()  # convert rows and columns to a single row, like a list
     fig = plt.figure()
+    plt.tight_layout()
     axis = fig.add_subplot(111)
-    axis.scatter(hf, sf, facecolors=pixel_colors, marker='.')
-    axis.set_xlabel('Hue')
-    axis.set_ylabel('Saturation')
-    # axis.set_zlabel('Value')
-    st.pyplot(fig)
+    col4, col5 = st.columns(2)
+    with col4:  # projection: Hue x Saturation
+        axis.scatter(hf, vf, facecolors=pixel_colors, marker='.')
+        axis.set_xlabel('Hue')
+        axis.set_ylabel('Value')
+        st.pyplot(fig)
+
+    with col5:  # projection: Hue x Value
+        axis.scatter(sf, vf, facecolors=pixel_colors, marker='.')
+        axis.set_xlabel('Saturation')
+        axis.set_ylabel('Value')
+        st.pyplot(fig)
+
+    with st.container():
+        fig3d, ax = plt.subplots(figsize=(10, 10))
+        axis = fig3d.add_subplot(1, 1, 1, projection="3d")
+        axis.scatter(hf, sf, vf, facecolors=pixel_colors, marker='.')
+        axis.set_xlabel('Hue')
+        axis.set_ylabel('Saturation')
+        axis.set_zlabel('Value')
+        st.pyplot(fig3d)
+
